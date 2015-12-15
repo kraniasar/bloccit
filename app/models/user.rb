9846f7c1de82f4be6f137f306bc9a12926e74ced
Amanda <kraniasar@gmail.com>
2015-12-15 09:59:08 -0500

    Assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :format_name
 
 
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -18,4 +19,15 @@ class User < ActiveRecord::Base
 
 
    has_secure_password
+
+   def format_name
+     if name
+       name_array = []
+       name.split.each do |name_part|
+         name_array  << name_part.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,12 @@ RSpec.describe User, type: :model do
      it "should respond to email" do
        expect(user).to respond_to(:email)
      end
+
+      it "should format the user's name" do
+        user.name = "bloc user"
+        user.save
+        expect(user.name).to eq "Bloc User"
+      end
    end
 
    describe "invalid user" do
