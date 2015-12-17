ffa44f23bc58916c3e6a6bc25bc2e2c60886bf41
Amanda <kraniasar@gmail.com>
2015-12-17 15:54:18 -0500

    assignment 37 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
 
+  def create
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
