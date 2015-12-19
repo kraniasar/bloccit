851f79302edcfddbe1e0b0bb0d4cdb08f3d3c801
Amanda <kraniasar@gmail.com>
2015-12-19 17:19:25 -0500

    changes to assignment 38

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-    gravatar_id = Digest: :MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-  end
-
+  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+end
 end
