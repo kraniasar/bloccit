abf5827d10c1c4c7edea739215090bd7e3e733a3
Amanda <kraniasar@gmail.com>
2015-12-23 20:18:35 -0500

    fixes to assignment 38

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,25 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
 
-   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
 
-   validates :email,
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
 
 
-   has_secure_password
+  has_secure_password
+
+  def self.avatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#%7Bgravatar_id%7D.png?s=#%7Bsize%7D"
+  end  
 end
