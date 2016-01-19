f362daf41874ebeee94b20814ba0885a9a95312e
Amanda <kraniasar@gmail.com>
2016-01-18 19:26:17 -0500

    checkpoint-44-fixtest

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Comment, type: :model do
-  describe "attributes" do
+
     let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
     let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
     let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
@@ -14,28 +14,29 @@ RSpec.describe Comment, type: :model do
     it { should validate_presence_of(:body) }
     it { should validate_length_of(:body).is_at_least(5) }
 
-    it "should respond to body" do
+    describe "attributes" do
+       it "should respond to body" do
         expect(comment).to respond_to(:body)
        end
      end
 
 
      describe "after_create" do
-     before do
-       @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+       before do
+         @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+       end
+
+       it "sends an email to users who have favorited the post" do
+         favorite = user.favorites.create(post: post)
+         expect(FavoriteMailer).to receive(:new_comment).with(user, post, @another_comment).and_return(double(deliver_now: true))
+
+         @another_comment.save
+       end
+
+       it "does not send emails to users who haven't favorited the post" do
+         expect(FavoriteMailer).not_to receive(:new_comment)
+
+         @another_comment.save
+       end
      end
-
-     it "sends an email to users who have favorited the post" do
-       favorite = user.favorites.create(post: post)
-       expect(FavoriteMailer).to receive(:new_comment).with(user, post, @another_comment).and_return(double(deliver_now: true))
-
-       @another_comment.save
-     end
-
-     it "does not send emails to users who haven't favorited the post" do
-       expect(FavoriteMailer).not_to receive(:new_comment)
-
-       @another_comment.save
-     end
-   end
  end
