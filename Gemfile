a9c7e2c6998bdff092be63d4ba39dd8a2cbd481b
Amanda <kraniasar@gmail.com>
2016-01-18 21:57:55 -0500

    checkpoint-45-publicprofiles-completed

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   gem 'shoulda'
-  gem 'pry-rails'
+  gem 'factory_girl_rails', '~> 4.0'
 end
 
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
+  end
+
   def create
 
     @user = User.new
