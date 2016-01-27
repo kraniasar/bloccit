a2812600724ba38430fcbb27204e1a16da548e3c
Brittany Martin <brittany.jill.martin@gmail.com>
2016-01-26 21:51:14 -0500

    Fixed up tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,47 +3,43 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
 
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
-   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-   validates :password, length: { minimum: 6 }, allow_blank: true
-
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 100 },
-             format: { with: EMAIL_REGEX }
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 100 },
+            format: { with: EMAIL_REGEX }
 
 
-   has_secure_password
+  has_secure_password
 
-   enum role: [:member, :admin]
+  enum role: [:member, :admin]
 
-   def favorite_for(post)
-     favorites.where(post_id: post.id).first
-   end
+  def favorite_for(post)
+    favorites.where(post_id: post.id).first
+  end
 
-   def avatar_url(size)
-     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
-     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-   end
+  def self.avatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 
-   def has_posts?
-     posts.count > 0
-   end
+  def has_posts?
+    posts.count > 0
+  end
 
-   def has_comments?
-     comments.count > 0
-   end
+  def has_comments?
+    comments.count > 0
+  end
 
-   def has_favorites?
-     favorites.count > 0
-   end
+  def has_favorites?
+    favorites.count > 0
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
-include RandomData
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
-  it { should have_many(:posts) }
+  it { should have_many(:posts)}
   it { should have_many(:comments) }
   it { should have_many(:votes) }
   it { should have_many(:favorites) }
-
+  # Shoulda tests for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
 
+  # Shoulda tests for email
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_length_of(:email).is_at_least(3) }
   it { should allow_value("user@bloccit.com").for(:email) }
   it { should_not allow_value("userbloccit.com").for(:email) }
 
-
+  # Shoulda tests for password
   it { should validate_presence_of(:password) }
   it { should have_secure_password }
   it { should validate_length_of(:password).is_at_least(6) }
@@ -94,69 +94,62 @@ RSpec.describe User, type: :model do
     end
   end
 
-
   describe "#favorite_for(post)" do
     before do
-      topic = Topic.create!(name: RandomData.random_sentence, description: Random.random_paragraph)
+      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
     end
 
-      it "returns `nil` if the user has not favorited the post" do
-
-        expect(user.favorite_for(@post)).to be_nil
-      end
-
-      it "returns the appropriate favorite if it exists" do
-
-        favorite = user.favorites.where(post: @post).create
-
-        expect(user.favorite_for(@post)).to eq(favorite)
-      end
+    it "returns `nil` if the user has not favorited the post" do
+      expect(user.favorite_for(@post)).to be_nil
     end
 
-    describe ".avatar_url" do
-
-      let(:known_user) { create(:user, email: "blochead@bloc.io") }
-
-      it "returns the proper Gravatar url for a known email entity" do
-        expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
-
-        expect(known_user.avatar_url(48)).to eq(expected_gravatar)
-      end
-    end
-
-    describe "#has_posts" do
-      it "returns false if the user doesn't have associated posts" do
-        expect(user.has_posts?).to eq false
-      end
-
-      it "returns true if the user has associated posts" do
-        user.posts << build(:post, user: user)
-        expect(user.has_posts?).to eq true
-      end
-    end
-
-    describe "#has_comments" do
-      it "returns false if the user doesn't have associated comments" do
-        expect(user.has_comments?).to eq false
-      end
-
-      it "returns true if the user has associated comments" do
-        user.posts << build(:post, user: user)
-        user.post.first.comments << build(:comment, user: user)
-        expect(user.has_comments?).to eq true
-      end
-    end
-
-    describe "#has_favorites" do
-      it "returns false if the user doesn't have associated favorites" do
-        expect(user.has_favorites?).to eq false
-      end
-
-      it "returns true if the use has associated favorites" do
-        post - build(:post, user: user)
-        user.favorites << Favorite.create!(post: post)
-        expect(user.has_favorites?).to eq true
-      end
+    it "returns the appropriate favorite if it exists" do
+      favorite = user.favorites.where(post: @post).create
+      expect(user.favorite_for(@post)).to eq(favorite)
     end
   end
+
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(User.avatar_url(known_user, 48)).to eq(expected_gravatar)
+    end
+  end
+
+  describe "#has_posts" do
+    it "returns false if the user doesn't have associated posts" do
+      expect(user.has_posts?).to eq false
+    end
+
+    it "returns true if the user has associated posts" do
+      user.posts << build(:post, user: user)
+      expect(user.has_posts?).to eq true
+    end
+  end
+
+  describe "#has_comments" do
+    it "returns false if the user doesn't have associated comments" do
+      expect(user.has_comments?).to eq false
+    end
+
+    it "returns true if the user has associated comments" do
+      user.posts << build(:post, user: user)
+      user.posts.first.comments << build(:comment, user: user)
+      expect(user.has_comments?).to eq true
+    end
+  end
+
+  describe "#has_favorites" do
+    it "returns false if the user doesn't have associated favorites" do
+      expect(user.has_favorites?).to eq false
+    end
+
+    it "returns true if the user has associated favorites" do
+      post = build(:post, user: user)
+      user.favorites << Favorite.create!(post: post)
+      expect(user.has_favorites?).to eq true
+    end
+  end
+end
