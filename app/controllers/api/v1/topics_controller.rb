9d05ac5a43039b4890b4dd2db57f62ff6bf0604e
Amanda <kraniasar@gmail.com>
2016-02-01 23:11:07 -0500

    checkpoint 48 api errors

---
+++ app/controllers/api/v1/topics_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::TopicsController < Api::V1::BaseController
+
+  before_action :authenticate_user, expect: [:index, :show]
+  before_action :authorize_user, expect: [:index, :show]
+
+  def index
+    topics = Topic.all
+    render json: topics.to_json, status: 200
+  end
+
+  def show
+    topic = Topic.find(params[:id])
+    render json: topic.to_json, status: 200
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
 
+  before_create :generate_auth_token
+
 
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -34,4 +36,11 @@ class User < ActiveRecord::Base
      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
    end
-end
+
+   def generate_auth_token
+     loop do
+       self.auth_token = SecureRandom.base64(64)
+       break unless User.find_by(auth_token: auth_token)
+     end
+   end
+ end
